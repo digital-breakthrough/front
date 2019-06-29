5063a6ea31f5a79a3276a5e24de8aac9128da9f3
Ladanov Nikolay <ladanov_n@mail.ru>
2019-06-29 18:16:05 +0300

    for cpecialist page

--- components/Specialist/Courses/index.js
+++ components/Specialist/Courses/index.js
@@ -1,10 +1,44 @@
 import React from "react";
 
 import './index.scss';
+const courseList = [{
+    id: 1,
+    title: 'Algorithmic Toolbox',
+    description: 'The class covers basic algorithmic techniques and ideas for computational problems arising frequently in practical applications: sorting and searching, divide and conquer, greedy algorithms, dynamic programming, and many others, You will learn how to sort and search data, how to break a large problem into pieces and solve them recursively; how to pack your knapsack using greedy algorithms, and how to search for similarities between genes using dynamic programming. You will practice solving computational problems, designing new algorithms, and implementing them as efficient software tools.',
+    specialisation: 'IT'
+}, {
+    id: 2,
+    title: 'Algorithmic Toolbox',
+    description: 'The class covers basic algorithmic techniques and ideas for computational problems arising frequently in practical applications: sorting and searching, divide and conquer, greedy algorithms, dynamic programming, and many others, You will learn how to sort and search data, how to break a large problem into pieces and solve them recursively; how to pack your knapsack using greedy algorithms, and how to search for similarities between genes using dynamic programming. You will practice solving computational problems, designing new algorithms, and implementing them as efficient software tools.',
+    specialisation: 'IT'
+}, {
+    id: 3,
+    title: 'Algorithmic Toolbox',
+    description: 'The class covers basic algorithmic techniques and ideas for computational problems arising frequently in practical applications: sorting and searching, divide and conquer, greedy algorithms, dynamic programming, and many others, You will learn how to sort and search data, how to break a large problem into pieces and solve them recursively; how to pack your knapsack using greedy algorithms, and how to search for similarities between genes using dynamic programming. You will practice solving computational problems, designing new algorithms, and implementing them as efficient software tools.',
+    specialisation: 'IT'
+}];
 
 const SpecialistCourses = () => (
 	<div className="specialist-course">
-		<h1>Something</h1>
+		<div className="wrap">
+            <h2>Подходящие курсы</h2>
+            <div className="list">
+                {
+                    courseList.map(course => {
+                        return (
+                            <div key={course.id} className="item">
+                                <div className="title">
+                                    {course.title}
+                                </div>
+                                <div className="description">
+                                    {course.description}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
 	</div>
 );
 
--- components/Specialist/Cover/index.js
+++ components/Specialist/Cover/index.js
@@ -1,13 +1,138 @@
 import React from "react";
-
+import Autosuggest from "react-autosuggest";
 import SpecialistCoverImg from "../../../static/specialistCover.jpg";
+import Link from "next/link";
 
-import './index.scss';
+import "./index.scss";
 
-const SpecialistCover = () => (
-	<div className="specialist-cover">
-		<img src={SpecialistCoverImg} />
-	</div>
+const specialisations = [
+  {
+    id: 0,
+    label: "Системный архитектор"
+  },
+  {
+    id: 1,
+    label: "Программист java"
+  },
+  {
+    id: 2,
+    label: "Программист c#"
+  },
+  {
+    id: 3,
+    label: "Программист php"
+  },
+  {
+    id: 4,
+    label: "Программист c"
+  },
+  {
+    id: 5,
+    label: "Программист oracle"
+  },
+  {
+    id: 6,
+    label: "Программист sql"
+  },
+  {
+    id: 7,
+    label: "Программист 1c"
+  },
+  {
+    id: 8,
+    label: "Системный аналитик"
+  },
+  {
+    id: 9,
+    label: "Тестировщик по"
+  },
+  {
+    id: 10,
+    label: "Менеджер информационного отдела"
+  }
+];
+
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : specialisations.filter(
+        lang => lang.label.toLowerCase().slice(0, inputLength) === inputValue
+      );
+};
+
+const getSuggestionValue = suggestion => suggestion.label;
+const renderSuggestion = suggestion => (
+  <div key={suggestion.id}>{suggestion.label}</div>
 );
+
+class SpecialistCover extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+	const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: 'Например, программист',
+      value,
+      onChange: this.onChange
+	};
+	
+    return (
+      <div className="specialist-cover">
+        <img src={SpecialistCoverImg} />
+        <div className="title">Для специалистов</div>
+        <div className="select-specialisation">
+			<label>
+				Укажи свою профессию
+			</label>
+			<Autosuggest
+				suggestions={suggestions}
+				onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+				onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+				getSuggestionValue={getSuggestionValue}
+				renderSuggestion={renderSuggestion}
+				inputProps={inputProps}
+		/>
+		</div>
+        <div className="subtitle">
+          Покажи на сколько ты крут, подтверди свои компетенции
+        </div>
+		<Link href="/#">
+			<a className="btn-test">
+				Подтвердить
+			</a>
+		</Link>
+      </div>
+    );
+  }
+}
 
 export default SpecialistCover;
